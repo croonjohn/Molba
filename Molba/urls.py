895c61e838054fdadd8d0b0b5a245ba008253d1d
croonjohn <tmdduf430@naver.com>
2018-12-26 15:03:40 +0900

    minor change

--- Molba/urls.py
+++ Molba/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from settings import base
+from .settings import base
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
